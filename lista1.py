f3d4d1184dafe4b094329ef874cd332cc91adffb
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-08-23 19:02:47 -0300

    Adicionando busca por interpolação
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- lista1.py
+++ lista1.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os 
+import os
 import time
 
 def busca_sequencial_padrao(vetor,numero_desejado):
@@ -27,10 +27,10 @@ def busca_sequencial_sentinela(vetor,numero_desejado):
         elapsed_time = time.time() - start_time
         print('Tempo de busca sequencial com sentinela: ' + str(elapsed_time) + ' s')
         return contador
-    
+
 def busca_binaria(vetor,numero_desejado):
     start_time = time.time()
-    valor_inicial = 0 
+    valor_inicial = 0
     valor_final = len(vetor)-1
     valor_medio = valor_final/2
     for i in range(len(vetor)):
@@ -48,6 +48,16 @@ def busca_binaria(vetor,numero_desejado):
             elapsed_time = time.time() - start_time
             print('Tempo de busca binaria: ' + str(elapsed_time) + ' s')
             return 'Valor nao encontrado!'
+
+def busca_interpolacao(vetor, numero_desejado):
+    start_time = time.time()
+    meio = ((len(vetor)-1) * (numero_desejado - vetor[0]))/(vetor[len(vetor)-1] - vetor[0])
+    elapsed_time = time.time() - start_time
+    print('Tempo de busca por interpolacao: ' + str(elapsed_time) + ' s')
+    if vetor[meio] == numero_desejado:
+        return meio
+    else:
+        return'Valor nao encontrado!'
 
 def preenche_vetor_desordenado(vetor):
     for i in range(0,10):
@@ -98,6 +108,7 @@ def buscar_vetor_ordenado():
     posicao = busca_sequencial_padrao(vetor_ordenado,valor)
     busca_sequencial_sentinela(vetor_ordenado,valor)
     busca_binaria(vetor_ordenado,valor)
+    busca_interpolacao(vetor_ordenado, valor)
     print('')
     print('O elemento esta na posicao: ' + str(posicao))
 
